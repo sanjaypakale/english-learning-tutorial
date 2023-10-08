e9a3030c92d11055d2172e5397e31474c004f2c5
sanjay pakale <sanjaypakale@gmail.com>
2023-10-08 21:23:57 +0800

    core java

--- src/app/java-interview/interview-dashboard/interview-dashboard.component.ts
+++ src/app/java-interview/interview-dashboard/interview-dashboard.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { DialogService } from 'src/app/dialog.service';
-
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-interview-dashboard',
   templateUrl: './interview-dashboard.component.html',
   styleUrls: ['./interview-dashboard.component.css']
 })
 export class InterviewDashboardComponent {
-  constructor(private dialogService: DialogService) { }
+  safeUrl: SafeResourceUrl;
+  constructor(private dialogService: DialogService, private sanitizer: DomSanitizer) { 
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/static-files/core-java.html');
+  }
 
   openHtmlFileDialog() {
     // Replace with the path to your HTML file
